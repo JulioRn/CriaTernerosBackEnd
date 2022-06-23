839931a6572232b814f33222240fe516a766a0a6
JulioRn <julio.roldan@estudiantes.utec.edu.uy>
2022-06-22 21:08:26 -0300

    22/6/2022

--- src/main/java/com/backend/CriaTernerosBackEnd/Servicios/RefractrometriaServicio.java
+++ src/main/java/com/backend/CriaTernerosBackEnd/Servicios/RefractrometriaServicio.java
@@ -1,7 +1,9 @@
 package com.backend.CriaTernerosBackEnd.Servicios;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.backend.CriaTernerosBackEnd.Modelo.Usuario;
 import org.springframework.stereotype.Service;
 
 import com.backend.CriaTernerosBackEnd.Modelo.Refractrometria;
@@ -11,6 +13,10 @@ public interface RefractrometriaServicio {
 	public Refractrometria saveRefractrometria(Refractrometria refractrometria);
 	public List<Refractrometria> getAllRefractrometrias();
 
+	public Optional<Refractrometria> findById(int id_refractrometria);
+
 	    public void removeRefractrometria(int id_refractrometria);
+
+	public Refractrometria findByTernId(int ternId);
 	
 }
